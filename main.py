fc9e90ffd2ec98da68ccc00c2345fd59c0ed0a83
NorternlakeNL <tomnoordermeer@live.nl>
2023-04-13 09:19:11 +0200

    delen worden herkent, niet alles - Tom

--- main.py
+++ main.py
@@ -1,56 +1,65 @@
-# import numpy as np
-# import pyautogui
-# import time
-# from PIL import ImageGrab
-# import cv2 as cv
-# import os
-
-# while True:
-#     filename = "img.png"
-#     for file in os.listdir("./"):
-#         if file == "img.png":
-#             os.remove(filename)
-#             print(f"removed {filename}")
-#     time.sleep(10)
-#     img = pyautogui.screenshot()
-#     img = cv.cvtColor(np.array(img), cv.COLOR_RGB2BGR)
-#     cv.imwrite(filename, img)
-#     print(f"made {filename}")
-#     time.sleep(10)
-
-
 import cv2 as cv
 import numpy as np
-from matplotlib import pyplot as plt
 import os
 
-image1 = cv.imread("image.png")
-for i in os.listdir("images"):
-    hsv_img = cv.cvtColor(i, cv.COLOR_BGR2HSV)
-    s_hc = hsv_img[:,:,1]
-    thesh = cv.threshold(s_hc, 5, 255, cv.THRESH_BINARY)[1]
-    thesh = cv.morphologyEx(thesh, cv.MORPH_OPEN, cv.getStructuringElement(cv.MORPH_ELLIPSE, (7,7)))
+img_dir = "images"
+floor_img = "image.png"
+floor = cv.imread(floor_img, cv.IMREAD_UNCHANGED)
 
-    cv.floodfill(thesh, None, seedPoint=(0,0), newVal=128, loDiff=1, upDiff=1)
-    
-    i[thesh == 128] = (255,255,255)
+for file in os.listdir(img_dir):
+    image_name = os.path.join(img_dir, file)
+    if not os.path.exists(image_name):
 
-    cv.imshow("i", i)
-    cv.waitKey(0)
+        print(f"{image_name} does not exist")
+        continue
+    img = cv.imread(image_name, cv.IMREAD_UNCHANGED)
+    img = img[275:250+300, 250:250+250]
 
-    # image2 = i
-    # cropped = image2[90:280, 150:330]
-    # print(image2.shape[+1::-1])
-    # w,h = image2.shape[+1::-1]
+    mask = img[:,:,3] == 0
+    img[mask] = [0,0,0,255]
 
-    # res = cv.matchTemplate(image1,image2,cv.TM_CCOEFF_NORMED)
-    # threshold = 0.5
-    # loc = np.where( res>=threshold)
-    # for pt in zip(*loc[::1]):
-    #     print(pt)
-    #     cv.rectangle(image1, pt, (pt[0] +w, pt[1] +h), (0,0,255), 2)
-    #     break
+    w,h = img.shape[+1::-1]
+    res = cv.matchTemplate(floor, img, cv.TM_CCOEFF_NORMED)
+    threshold = 0.5
+    loc = np.where( res>=threshold )
 
-    # cv.imshow('res.png', image1)
-    # cv.imshow('search.png', image2)
-    # cv.waitKey(0)
+    floor_copy = floor.copy()
+    for pt in zip(*loc[::-1]):
+        match_area = floor[pt[1]:pt[1]+h, pt[0]:pt[0]+w]
+        match_gray = cv.cvtColor(match_area, cv.COLOR_BGR2GRAY)
+        img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        _, match_thresh = cv.threshold(match_gray, 0, 255, cv.THRESH_BINARY_INV)
+        _, img_thresh = cv.threshold(img_gray, 0, 255, cv.THRESH_BINARY_INV)
+
+        match_cont, _ = cv.findContours(match_thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        img_cont, _ = cv.findContours(img_thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+        match_rect = cv.minAreaRect(match_cont[0])
+        match_box = cv.boxPoints(match_rect)
+        match_box = np.intp(match_box)
+
+        img_rect = cv.minAreaRect(img_cont[0])
+        img_box = cv.boxPoints(img_rect)
+        img_box = np.intp(img_box)
+
+        affaine = cv.getAffineTransform( img_box[:3].astype(np.float32).reshape(3,2), match_box[:3].astype(np.float32).reshape(3,2))
+        warped = cv.warpAffine(img, affaine, (w,h), borderMode=cv.BORDER_CONSTANT, borderValue=(0,0,0,0))
+
+        floor_copy[pt[1]:pt[1]+h, pt[0]:pt[0]+w] = cv.addWeighted(warped, 1, match_area, 1 ,0)
+
+        cv.drawContours(floor_copy, [match_box], 0, (0,0,255), 1)
+
+        cv.rectangle(floor_copy, pt, (pt[0]+w, pt[1]+h), (0,0,255), 1)
+        break
+
+    cv.imshow(f'floor', floor_copy)
+    cv.imshow(f'{image_name}', img)
+    key = cv.waitKey(0) & 0xFF
+    if key == 27:
+        break
+    elif key == 13:
+        cv.destroyWindow(f'{image_name}')
+        cv.destroyWindow(f'floor')
+
+
+cv.destroyAllWindows()
--- temp.py
+++ temp.py
@@ -1,24 +1,25 @@
 #test 1
-# import cv2
-# import numpy as np
-# from matplotlib import pyplot as plt
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
-# image1 = cv2.imread("image.png")
-# image2 = cv2.imread("image5.png")
-# print(image2.shape[+1::-1])
-# w,h = image2.shape[+1::-1]
+image1 = cv2.imread("image.png")
+image2 = cv2.imread("images/image7.png")
+print(image2.shape[+1::-1])
+image2 = image2[275:250+300, 250:250+250]
+w,h = image2.shape[+1::-1]
 
-# res = cv2.matchTemplate(image1,image2,cv2.TM_CCOEFF_NORMED)
-# threshold = 0.6
-# loc = np.where( res>=threshold)
-# for pt in zip(*loc[::1]):
-#     print(pt)
-#     cv2.rectangle(image1, pt, (pt[0] +w, pt[1] +h), (0,0,255), 2)
-#     break
+res = cv2.matchTemplate(image1,image2,cv2.TM_CCORR_NORMED)
+threshold = 0.5
+loc = np.where( res>=threshold)
+for pt in zip(*loc[::-1]):
+    print(pt)
+    cv2.rectangle(image1, pt, (pt[0] +w, pt[1] +h), (0,0,255), 2)
+    break
 
-# cv2.imshow('res.png', image1)
-# cv2.imshow('search.png', image2)
-# cv2.waitKey(0)
+cv2.imshow('res.png', image1)
+cv2.imshow('search.png', image2)
+cv2.waitKey(0)
 
 #test 2
 # import numpy as np
@@ -48,4 +49,53 @@
 # screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
 
 # cv.imshow("screenshot", screenshot)
+# cv.waitKey(0)
+
+
+#test 4
+# import cv2 as cv
+# import numpy as np
+# from matplotlib import pyplot as plt
+# import os
+
+# image1 = cv.imread("image.png")
+# for i in os.listdir("images"):
+#     hsv_img = cv.cvtColor(i, cv.COLOR_BGR2HSV)
+#     s_hc = hsv_img[:,:,1]
+#     thesh = cv.threshold(s_hc, 5, 255, cv.THRESH_BINARY)[1]
+#     thesh = cv.morphologyEx(thesh, cv.MORPH_OPEN, cv.getStructuringElement(cv.MORPH_ELLIPSE, (7,7)))
+
+#     cv.floodfill(thesh, None, seedPoint=(0,0), newVal=128, loDiff=1, upDiff=1)
+    
+#     i[thesh == 128] = (255,255,255)
+
+#     cv.imshow("i", i)
+#     cv.waitKey(0)
+
+#     image2 = i
+#     cropped = image2[90:280, 150:330]
+#     print(image2.shape[+1::-1])
+#     w,h = image2.shape[+1::-1]
+
+#     res = cv.matchTemplate(image1,image2,cv.TM_CCOEFF_NORMED)
+#     threshold = 0.5
+#     loc = np.where( res>=threshold)
+#     for pt in zip(*loc[::1]):
+#         print(pt)
+#         cv.rectangle(image1, pt, (pt[0] +w, pt[1] +h), (0,0,255), 2)
+#         break
+
+#     cv.imshow('res.png', image1)
+#     cv.imshow('search.png', image2)
+#     cv.waitKey(0)
+
+# test 5
+# import cv2 as cv
+# import numpy as np
+
+# img = cv.imread("images/image3.png", cv.IMREAD_UNCHANGED)
+# mask = img[:,:,3] == 0
+# img[mask] = [0,0,0,255]
+
+# cv.imshow('img', img)
 # cv.waitKey(0)
