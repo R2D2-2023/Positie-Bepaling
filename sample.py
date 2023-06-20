7ddd386500e442faf5fe3b9a8f55361a779d5712
Benjamin pi <benjaminboehmer@live.nl>
2023-06-20 14:34:51 +0200

    splitted the degrees from distance

--- sample.py
+++ sample.py
@@ -8,9 +8,23 @@ if not lidar.open():
     exit(1)
 
 t = time.time()
-while time.time() - t < 20:  # Run for 20 seconds
+# while time.time() - t < 0.5:  # Run for 20 seconds
+lijst=[]
+for i in range(22):
     measures = lidar.getMeasures()  # Get latest lidar measures
-    print(measures)
-    time.sleep(0.01)
-
+    # print(measures)
+    # print(len(measures))    
+    time.sleep(0.015)
+    if len(measures)>=270:
+        lijst.extend(measures)
+        measures=[]
+print(lijst)
+print(len(lijst))
 lidar.close()
+
+for i in lijst:
+    x=str(i).split(":")    
+    degree=float(x[0])
+    distance=float(x[1].split("mm")[0])
+    print(f"afstand is {distance} en degree is {degree} \n")
+
