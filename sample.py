79d5d6507427a4a50ef64a5ffb678733f197b5e8
Benjamin pi <benjaminboehmer@live.nl>
2023-06-20 16:14:44 +0200

    drawing the dots on the degree and distance
    Co-authored-by: Ilias-a <Ilias-a@users.noreply.github.com>

--- sample.py
+++ sample.py
@@ -1,7 +1,17 @@
 from test import LidarX2
 import time
-
+import numpy
+import cv2
+import math
 lidar = LidarX2("/dev/ttyUSB0")  # Name of the serial port, can be /dev/tty*, COM*, etc.
+
+def calcPos(posX, posY, rotation, distance):
+    y_Offset = math.cos(rotation * 3.14159265359 / 180) * (distance*200)
+    x_Offset = math.sin(rotation * 3.14159265359 / 180) * (distance*200)
+    posX += x_Offset
+    posY += -y_Offset
+    return (int(posX)-320, int(posY)-240)
+
 
 if not lidar.open():
     print("Cannot open lidar")
@@ -14,17 +24,26 @@ for i in range(22):
     measures = lidar.getMeasures()  # Get latest lidar measures
     # print(measures)
     # print(len(measures))    
-    time.sleep(0.015)
     if len(measures)>=270:
         lijst.extend(measures)
-        measures=[]
-print(lijst)
-print(len(lijst))
-lidar.close()
+        # measures=[]    
+    time.sleep(0.015)
 
+# print(lijst)
+# print(len(lijst))
+img = numpy.ones((480, 640))
+lidar.close()
 for i in lijst:
     x=str(i).split(":")    
     degree=float(x[0])
     distance=float(x[1].split("mm")[0])
     print(f"afstand is {distance} en degree is {degree} \n")
+    img = cv2.circle(img, calcPos(640, 480, degree, distance), 1, (0, 0, 0), 2)
+
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+
+print(degree)
+print(distance)
+print(img)
 
