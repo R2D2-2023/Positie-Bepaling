08351d14bd358b7fcd90c5728c98d1eb9fce23e5
Benjamin pi <benjaminboehmer@live.nl>
2023-06-21 11:02:07 +0200

    small clean up

--- sample.py
+++ sample.py
@@ -16,23 +16,14 @@ while True:
     if not lidar.open():
         print("Cannot open lidar")
         exit(1)
-
     t = time.time()
-    # while time.time() - t < 0.5:  # Run for 20 seconds
     lijst=[]
     for i in range(22):
         measures = lidar.getMeasures()  # Get latest lidar measures
-        # print(measures)
-        # print(len(measures))    
         if len(measures)>=270:
             lijst.extend(measures)
-            # measures=[]    
         time.sleep(0.015)
     lidar.close()
-
-    # print(lijst)
-    # print(len(lijst))
-
     distance=[]
     degrees=[]
     img = numpy.ones((480, 640))
@@ -40,19 +31,9 @@ while True:
         x=str(i).split(":")    
         degrees.append(float(x[0]))
         distance.append(float(x[1].split("mm")[0]))
-        # print(f"afstand is {distance} en degree is {degrees[]} \n")
-    # print(degrees)
-    # print(distance)
     for i in range(0, len(distance)-1):
         distance[i] = distance[i] / 10000
-
-
     for i in range(0, len(degrees)-1):
         img = cv2.circle(img, calcPos(640, 480, degrees[i], distance[i]), 1, (0, 0, 0), 2)
-
     cv2.imshow("Image", img)
     cv2.waitKey(1)
-
-    # print(degrees)
-    # print(distance)
-    # print(img)
