a164c6620b70147e124724d27b08770a930465cd
Benjamin pi <benjaminboehmer@live.nl>
2023-06-22 11:22:25 +0200

    Made new pictures for matching(benjamin, illias en tjez)
    Co-authored-by: Ilias-a <Ilias-a@users.noreply.github.com>

--- sample.py
+++ sample.py
@@ -3,20 +3,25 @@ import time
 import numpy
 import cv2
 import math
+count=0
 lidar = LidarX2("/dev/ttyUSB0")  # Name of the serial port, can be /dev/tty*, COM*, etc.
 def calcPos(posX, posY, rotation, distance):
     y_Offset = math.cos(rotation * 3.14159265359 / 180) * (distance*1000)
     x_Offset = math.sin(rotation * 3.14159265359 / 180) * (distance*1000)
     posX += x_Offset
     posY += -y_Offset
-    return (int(posX)-100, int(posY)-100)
-count=0
+    return (int(posX)-150, int(posY)-150)
+
+
 while True:
+    #starts the lidar
     if not lidar.open():
         print("Cannot open lidar")
         exit(1)
-    t = time.time()
+
     lijst=[]
+    #reads the lidar data and saves it in a list
+
     for i in range(15):
         measures = lidar.getMeasures()  # Get latest lidar measures
         if len(measures)>=270:
@@ -24,23 +29,28 @@ while True:
             measures=[]
         time.sleep(0.015)
     lidar.close()
+
+
     distance=[]
     degrees=[]
-    img = numpy.ones((200, 350))
+    img = numpy.ones((300, 300))
     img*=0.7
+    #this makes 2 seperate lists, one with the degrees and one with the distance
     for i in lijst:
         x=str(i).split(":")
         if float(float(x[1].split("mm")[0]))!=0:    
             degrees.append(float(x[0]))
             distance.append(float(x[1].split("mm")[0]))
+    #this makes mm to pixels between the middle and points
     for i in range(0, len(distance)-1):
         distance[i] = distance[i] / 30000
+    #this draws the lines and points
     for i in range(0, len(degrees)-1):
-        img = cv2.line(img, (100, 100), calcPos(200, 200, degrees[i], distance[i]), (1, 1, 1), 2)
-        img = cv2.circle(img, calcPos(200, 200, degrees[i], distance[i]), 1, (0, 0, 0), 2)
-
-
+        img = cv2.line(img, (150, 150), calcPos(300, 300, degrees[i], distance[i]), (1, 1, 1), 6)
+        img = cv2.circle(img, calcPos(300, 300, degrees[i], distance[i]), 1, (0, 0, 0), 6)
+    #this shows the image
     cv2.imshow("Image", img)
+    #this saves the image if a button is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         count+=1
         cv2.imwrite("test_pictures"+'/'+str(count)+".png",255*img)
