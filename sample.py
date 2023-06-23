fa28df0f467778a8892369cedf64d428385c691f
Benjamin pi <benjaminboehmer@live.nl>
2023-06-23 12:41:38 +0200

    Changed some values to get a better image capture and removed old pictures Co-authored-by: Ilias-a <Ilias-a@users.noreply.github.com>

--- sample.py
+++ sample.py
@@ -6,53 +6,61 @@ import math
 count=0
 lidar = LidarX2("/dev/ttyUSB0")  # Name of the serial port, can be /dev/tty*, COM*, etc.
 def calcPos(posX, posY, rotation, distance):
-    y_Offset = math.cos(rotation * 3.14159265359 / 180) * (distance*1000)
-    x_Offset = math.sin(rotation * 3.14159265359 / 180) * (distance*1000)
+    y_Offset = math.cos(rotation * 3.14159265359 / 180) * (distance*200)
+    x_Offset = math.sin(rotation * 3.14159265359 / 180) * (distance*200)
     posX += x_Offset
     posY += -y_Offset
-    return (int(posX)-150, int(posY)-150)
+    return (int(posX)-300, int(posY)-300)
 
+# if not lidar.open():
+#         print("Cannot open lidar")
+#         exit(1)
+lijst=[]
 
-while True:
+while lidar.open():
     #starts the lidar
-    if not lidar.open():
-        print("Cannot open lidar")
-        exit(1)
 
-    lijst=[]
     #reads the lidar data and saves it in a list
-
-    for i in range(15):
+    for i in range(20):
         measures = lidar.getMeasures()  # Get latest lidar measures
-        if len(measures)>=270:
-            lijst.extend(measures)
-            measures=[]
+        #if len(measures)>=270:
+        lijst.extend(measures)
+        measures=[]
         time.sleep(0.015)
     lidar.close()
-
+    # print(lijst)
 
     distance=[]
     degrees=[]
-    img = numpy.ones((300, 300))
-    img*=0.7
+    img = numpy.ones((600, 600))
+    img*=0.392
+    # print(f"{img}")
+    #print(len(lijst))
     #this makes 2 seperate lists, one with the degrees and one with the distance
     for i in lijst:
         x=str(i).split(":")
-        if float(float(x[1].split("mm")[0]))!=0:    
+        if float(float(x[1].split("mm")[0]))!=0 and float(x[0])>0 :    
             degrees.append(float(x[0]))
             distance.append(float(x[1].split("mm")[0]))
     #this makes mm to pixels between the middle and points
     for i in range(0, len(distance)-1):
-        distance[i] = distance[i] / 30000
-    #this draws the lines and points
+        distance[i] = distance[i] / 2000
+    #this draws the lines and points    
+    # img = numpy.ones((300, 300))
+
     for i in range(0, len(degrees)-1):
-        img = cv2.line(img, (150, 150), calcPos(300, 300, degrees[i], distance[i]), (1, 1, 1), 6)
-        img = cv2.circle(img, calcPos(300, 300, degrees[i], distance[i]), 1, (0, 0, 0), 6)
+        img = cv2.line(img, (300, 300), calcPos(600, 600, degrees[i], distance[i]), (1, 1, 1), 3)
+        img = cv2.circle(img, calcPos(600, 600, degrees[i], distance[i]), 1, (0, 0, 0), 3)
     #this shows the image
     cv2.imshow("Image", img)
+    lijst.clear()
     #this saves the image if a button is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         count+=1
+        img = cv2.resize(img, (90,90))
         cv2.imwrite("test_pictures"+'/'+str(count)+".png",255*img)
+
+    if cv2.waitKey(1) & 0xFF == ord('e'):
+        resized = cv2.resize(img, (90,90))
 
     cv2.waitKey(1)
