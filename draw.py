58abcb2acf33332bbdd59ab73f4e2e15a5035f5c
Northie <tomnoordermeer@live.nl>
2023-06-27 15:48:23 +0200

    changed color

--- draw.py
+++ draw.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 
 # Load the image
@@ -5,12 +6,12 @@ image = cv2.imread('map_new_new_edited.png')
 
 # Define the dot position
 dot_x = 650
-dot_y = 900
+dot_y = 1500
 
 # Draw a dot on the image
-cv2.circle(image, (dot_x, dot_y), radius=2, color=(0, 0, 255), thickness=-1)
+cv2.circle(image, (dot_x, dot_y), radius=2, color=(255, 0, 0), thickness=-1)
 
 # Display the image with the dot
+if (os.path.exists('location.png')):
+    os.remove('location.png')
 cv2.imwrite('location.png', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
--- read.py
+++ read.py
@@ -3,19 +3,15 @@ import numpy as np
 
 with open('coords.txt', 'w') as f:
 
-
+    blue = (255,0,0)
     # Load the map image
     image = cv2.imread('location.png')
 
-    # Convert the image to HSV color space
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
     # Define the lower and upper bounds for the red color range
-    lower_red = np.array([0, 50, 50])
-    upper_red = np.array([10, 255, 255])
+    blue = (255,0,0)
 
     # Threshold the image to get only red pixels
-    mask = cv2.inRange(hsv, lower_red, upper_red)
+    mask = cv2.inRange(image, blue, blue)
 
     # Find contours in the binary image
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
