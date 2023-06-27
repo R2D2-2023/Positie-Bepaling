3899dd2bb84966043532565d93b9ce2dd1c9fdfd
Boehmer2003 <benjaminboehmer@live.nl>
2023-06-27 12:05:10 +0200

    live showing location

--- sample.py
+++ sample.py
@@ -15,6 +15,13 @@ def calcPos(posX, posY, rotation, distance):
     return (int(posX)-300, int(posY)-300)
 
 
+def rotateImage(image, angle):
+        row, col = image.shape
+        center = tuple(np.array([col, row]) / 2)  # Corrected order of dimensions
+        rot_mat = cv2.getRotationMatrix2D(center, angle, 1.0)
+        new_image = cv2.warpAffine(image, rot_mat, (col, row))
+        return new_image
+
 lijst=[]
 
 while lidar.open():
@@ -50,65 +57,83 @@ while lidar.open():
         img = cv2.line(img, (300, 300), calcPos(600, 600, degrees[i], distance[i]), (1, 1, 1), 3)
         img = cv2.circle(img, calcPos(600, 600, degrees[i], distance[i]), 1, (0, 0, 0), 4)
 
-    def rotateImage(image, angle):
-        row, col = image.shape
-        center = tuple(np.array([col, row]) / 2)  # Corrected order of dimensions
-        rot_mat = cv2.getRotationMatrix2D(center, angle, 1.0)
-        new_image = cv2.warpAffine(image, rot_mat, (col, row))
-        return new_image
-
-    img_map = cv2.imread('map/Map_West.png', cv2.IMREAD_GRAYSCALE)
+    img_map = cv2.imread('./Map_West.png', cv2.IMREAD_GRAYSCALE)
     assert img_map is not None, "file could not be read, check with os.path.exists()"
     img2 = img_map.copy()
 
-    template = img.copy()
-    template = cv2.resize(template, (int(template.shape[1] * 1.45), int(template.shape[0] * 1.35)))
+    img = cv2.resize(img, (100,100))
+    uint_img = np.array(img*255).astype('uint8')
+    template = uint_img.copy()
+    # template = cv2.resize(template, (int(template.shape[1] * 1.45), int(template.shape[0] * 1.35)))
 
     assert template is not None, "file could not be read, check with os.path.exists()"
+    # print(template.shape)
     w, h = template.shape[::-1]
 
     # All the 6 methods for comparison in a list
-    methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED']
+    methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED']
 
-    threshold = 0.45
+    threshold = 0.30
+    # img = cv2.resize(img, (90,90))
 
-    for rotation in range(0, 1):
-        rotated_template = rotateImage(template, rotation)
-        for meth in methods:
-            img_map = img2.copy()
-            method = eval(meth)
-            # Apply template Matching
-            res = cv2.matchTemplate(img_map, rotated_template, method)
-            loc = np.where(res >= threshold)
-            print(loc)
-            for pt in zip(*loc[::-1]):
-                cv2.rectangle(img_map, pt, (pt[0] + w, pt[1] + h), (0, 0, 0), 2)
-            # If statement vorige code terug zetten.
+    # for rotation in range(0, 1):
+    #     rotated_template = rotateImage(template, rotation)
+    for meth in methods:
+        img_map = img2.copy()
+        method = eval(meth)
+        # Apply template Matching
+        res = cv2.matchTemplate(img_map, template, method)
+        amount = 0 # of matches
+        step = 10
+        threshold = 0.1 #
+        loc = []
+        cycles = 0
+        loc = np.where(res >= threshold)
+        while(amount < 1 or amount > 1): #aslong as amount is above or beneath 1
+            loc = np.where( res>=threshold ) #look where the image matches the map
+            amount = len(loc[0]) #store how many are matching
+            if amount < 1:
+                threshold -= step
+            elif amount > 1:
+                threshold += step
+            step *= 0.9
+            cycles +=1
+            if cycles > 100:
+                break
 
-    # for meth in methods:
-    #     img = img2.copy()
-    #     method = eval(meth)
-    #     # Apply template Matching
-    #     res = cv.matchTemplate(img,template,method)
-    #     print(res)
-    #     min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-    #     # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    #     if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
-    #         top_left = min_loc
-    #     else:
-    #         top_left = max_loc
-    #     bottom_right = (top_left[0] + w, top_left[1] + h)
-    #     cv.rectangle(img,top_left, bottom_right, 0, 2)
-            
-    plt.subplot(2, 1)
-    plt.imshow(img_map, cmap='gray')
-    plt.title('Detected Point')
 
-    #this shows the image
-    plt.subplot(1, 1)
-    plt.imshow("Image", img)
-    plt.title('Live LIDAR Image')
-    plt.show()
+
+        # print(loc)
+        for pt in zip(*loc[::-1]):
+            cv2.rectangle(img_map, pt, (pt[0] + w, pt[1] + h), (0, 0, 0), 2)
+        # If statement vorige code terug zetten.
+
+    
+    while(amount < 1 or amount > 1): #aslong as amount is above or beneath 1
+
+        loc = np.where( res>=threshold ) #look where the image matches the map
+        amount = len(loc[0]) #store how many are matching
+        if amount < 1:
+            threshold -= step
+        elif amount > 1:
+            threshold += step
+        step *= 0.9
+        cycles +=1
+        if cycles > 100:
+            break
+
+
+
+    # fig, axs = plt.subplots( 1, 2 )
+    # axs[0].clear()
+    # axs[0].imshow( img_map, cmap='gray' )
+    # axs[1].clear()
+    # axs[1].imshow( img, cmap='gray' )
+    # plt.show()
+
+    cv2.imshow("LIDAR Image", img)
+    cv2.imshow("Check Image", img_map)
+
     lijst.clear()
 
     #this saves the image if a button is pressed
