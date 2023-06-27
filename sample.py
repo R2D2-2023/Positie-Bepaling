957a4d9302e6ceeac9e4852eef3c2ba79862c747
Boehmer2003 <benjaminboehmer@live.nl>
2023-06-27 16:18:18 +0200

    workin with diffrent maps
    Co-authored-by: Ilias-a <Ilias-a@users.noreply.github.com>
    Co-authored-by: Tjees <Tjees@users.noreply.github.com>
    Co-authored-by: Northie <northernlakeNL@users.noreply.github.com>

--- sample.py
+++ sample.py
@@ -24,6 +24,8 @@ def rotateImage(image, angle):
 
 lijst=[]
 
+map_perspective = input("Kant van de map: ")
+
 while lidar.open():
     #starts the lidar
     #reads the lidar data and saves it in a list
@@ -37,7 +39,7 @@ while lidar.open():
     distance=[]
     degrees=[]
     img = np.ones((600, 600))
-    img*=0.392
+    img*=0.8
     
     #this makes 2 seperate lists, one with the degrees and one with the distance
     for i in lijst:
@@ -51,13 +53,33 @@ while lidar.open():
             distance[i] = 2 # 4000/2000 = 4 meter 
         else:
             distance[i] = distance[i] / 2000
+        
     #this draws the lines and points    
 
     for i in range(0, len(degrees)-1):
-        img = cv2.line(img, (300, 300), calcPos(600, 600, degrees[i], distance[i]), (1, 1, 1), 3)
+        img = cv2.line(img, (300, 300), calcPos(600, 600, degrees[i], distance[i]), (1, 1, 1), 3)       
         img = cv2.circle(img, calcPos(600, 600, degrees[i], distance[i]), 1, (0, 0, 0), 4)
+        # if distance[i]== 0:
 
-    img_map = cv2.imread('./Map_West3.png', cv2.IMREAD_GRAYSCALE)
+    if( map_perspective == "N"):
+        img_map = cv2.imread('./Map_Noord.png', cv2.IMREAD_GRAYSCALE)
+        img_map = cv2.rotate(img_map, cv2.ROTATE_90_CLOCKWISE)
+    elif( map_perspective == "E"):
+        img_map = cv2.imread('./Map_Oost.png', cv2.IMREAD_GRAYSCALE)
+        print(img_map.shape)
+        img_map = cv2.resize(img_map, (100,img_map.shape[0]))
+    elif( map_perspective == "S"):
+        img_map = cv2.imread('./Map_Zuid3.png', cv2.IMREAD_GRAYSCALE)
+        img_map = cv2.rotate(img_map, cv2.ROTATE_90_CLOCKWISE)
+    elif( map_perspective == "W"):
+        img_map = cv2.imread('./Map_West.png', cv2.IMREAD_GRAYSCALE)
+        img_map = cv2.rotate(img_map, cv2.ROTATE_90_CLOCKWISE)
+        img_map = cv2.rotate(img_map, cv2.ROTATE_90_CLOCKWISE)
+
+
+
+        
+    # img_map = cv2.imread('./Map_Zuid3.png', cv2.IMREAD_GRAYSCALE)
     assert img_map is not None, "file could not be read, check with os.path.exists()"
     img2 = img_map.copy()
 
@@ -71,9 +93,9 @@ while lidar.open():
     w, h = template.shape[::-1]
 
     # All the 6 methods for comparison in a list
-    methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED']
+    methods = ['cv2.TM_CCOEFF_NORMED']
 
-    threshold = 0.30
+    # threshold = 0.30
     # img = cv2.resize(img, (90,90))
 
     # for rotation in range(0, 1):
@@ -85,7 +107,7 @@ while lidar.open():
         res = cv2.matchTemplate(img_map, template, method)
         amount = 0 # of matches
         step = 10
-        threshold = 0.1 #
+        threshold = 1 #
         loc = []
         cycles = 0
         loc = np.where(res >= threshold)
@@ -100,9 +122,9 @@ while lidar.open():
             cycles +=1
             if cycles > 100:
                 break
+        print(threshold)
 
-
-
+        
         # print(loc)
         for pt in zip(*loc[::-1]):
             cv2.rectangle(img_map, pt, (pt[0] + w, pt[1] + h), (0, 0, 0), 2)
@@ -131,8 +153,11 @@ while lidar.open():
     # axs[1].imshow( img, cmap='gray' )
     # plt.show()
     # image_map = cv2.resize(img_map, (int(img_map.shape[1] * 0.5), int(img_map.shape[0] * 0.5)))
+    img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
 
     cv2.imshow("LIDAR Image", img)
+    img_map = cv2.rotate(img_map, cv2.ROTATE_90_CLOCKWISE)
+
     cv2.imshow("Check Image", img_map)
 
     lijst.clear()
@@ -141,7 +166,7 @@ while lidar.open():
     if cv2.waitKey(1) & 0xFF == ord('q'):
         count+=1
         img = cv2.resize(img, (90,90))
-        cv2.imwrite(rechtsonder"+'/'+str(count)+".png",255*img)
+        cv2.imwrite("rechtsonder"+'/'+str(count)+".png",255*img)
 
     if cv2.waitKey(1) & 0xFF == ord('e'):
         resized = cv2.resize(img, (90,90))
